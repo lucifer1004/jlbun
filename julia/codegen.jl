52907fe47a3c39be8bb691182684916dc262dbf2
Gabriel Wu <wuzihua@pku.edu.cn>
2022-12-29 16:27:48 +0800

    use symbol whitelist instead of blacklist

--- julia/codegen.jl
+++ julia/codegen.jl
@@ -24,30 +24,78 @@ TYPEDEF_DICT = Dict(
     "jl_ptls_t" => "FFIType.ptr",
 )
 
-EXCLUDED = Set([
-    "jl_finalize",
-    "jl_free_stack",
-    "jl_gc_use",
-    "jl_clear_malloc_data",
-    "jl_symbol_name",
-    "jl_egal__unboxed",
-    "jl_apply_modify_type",
-    "jl_set_module_max_methods",
-    "jl_get_module_max_methods",
-    "jl_binding_type",
-    "jl_module_use_as",
-    "jl_module_import_as",
-    "jl_stat",
-    "jl_deserialize_verify_header",
-    "jl_set_newly_inferred",
-    "jl_expand_in_world",
-    "jl_is_syntactic_operator",
-    "jl_uv_puts",
-    "jl_flush_cstdio",
-    "jl_",
-    "jl_sizeof_jl_options",
-    "jl_format_filename"
+INCLUDED = Set([
+    "jl_apply_array_type",
+    "jl_alloc_array_1d",
+    "jl_ptr_to_array_1d",
+    "jl_arrayref",
+    "jl_arrayset",
+    "jl_array_ptr_1d_push",
+    "jl_array_eltype",
+    "jl_init",
+    "jl_init_with_image",
+    "jl_typeof_str",
+    "jl_eval_string",
+    "jl_exception_occurred",
+    "jl_call0",
+    "jl_call1",
+    "jl_call2",
+    "jl_call3",
+    "jl_call",
+    "jl_atexit_hook",
+    "jl_get_global",
+    "jl_get_nth_field",
+    "jl_box_int8",
+    "jl_box_uint8",
+    "jl_box_int16",
+    "jl_box_uint16",
+    "jl_box_int32",
+    "jl_box_uint32",
+    "jl_box_int64",
+    "jl_box_uint64",
+    "jl_box_float32",
+    "jl_box_float64",
+    "jl_box_bool",
+    "jl_unbox_int8",
+    "jl_unbox_uint8",
+    "jl_unbox_int16",
+    "jl_unbox_uint16",
+    "jl_unbox_int32",
+    "jl_unbox_uint32",
+    "jl_unbox_int64",
+    "jl_unbox_uint64",
+    "jl_unbox_float32",
+    "jl_unbox_float64",
+    "jl_unbox_bool",
+    "jl_cstr_to_string",
+    "jl_string_ptr",
+    "jl_symbol",
 ])
+
+# EXCLUDED = Set([
+#     "jl_finalize",
+#     "jl_free_stack",
+#     "jl_gc_use",
+#     "jl_clear_malloc_data",
+#     "jl_symbol_name",
+#     "jl_egal__unboxed",
+#     "jl_apply_modify_type",
+#     "jl_set_module_max_methods",
+#     "jl_get_module_max_methods",
+#     "jl_binding_type",
+#     "jl_module_use_as",
+#     "jl_module_import_as",
+#     "jl_stat",
+#     "jl_deserialize_verify_header",
+#     "jl_set_newly_inferred",
+#     "jl_expand_in_world",
+#     "jl_is_syntactic_operator",
+#     "jl_uv_puts",
+#     "jl_flush_cstdio",
+#     "jl_",
+#     "jl_sizeof_jl_options",
+#     "jl_format_filename"
+# ])
 
 function bunffi_type(cursor_type)
     result = @match cursor_type begin
@@ -82,7 +130,7 @@ function codegen(julia_header_path)
     root_cursor = Clang.getTranslationUnitCursor(trans_unit)
     fdecls = search(root_cursor, CXCursor_FunctionDecl)
 
-    bunffi_fdecls = map(filter(x -> isexported(x) && occursin(spelling(x), src) && spelling(x) ∉ EXCLUDED, fdecls)) do fdecl
+    bunffi_fdecls = map(filter(x -> isexported(x) && occursin(spelling(x), src) && spelling(x) ∈ INCLUDED, fdecls)) do fdecl
         name = spelling(fdecl)
         result_type = Clang.getCursorResultType(fdecl)
         returns = bunffi_type(result_type)
